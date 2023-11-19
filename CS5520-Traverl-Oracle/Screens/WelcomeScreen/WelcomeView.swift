01d95f7ed41b1d6a12974585d49a5139bbc5987a
ChangxuRen-github <changxuren.email@gmail.com>
2023-11-18 21:34:01 -0800

    Add welcome screen

---
+++ CS5520-Traverl-Oracle/Screens/WelcomeScreen/WelcomeView.swift
@@ -0,0 +1,50 @@
+//
+//  WelcomeView.swift
+//  CS5520-Traverl-Oracle
+//
+//  Created by Changxu Ren on 11/18/23.
+//
+
+import UIKit
+
+class WelcomeView: UIView {
+    // image height
+    let IMAGE_HEIGHT = CGFloat(200)
+    // image width
+    let IMAGE_WIDTH = CGFloat(250)
+    
+    // welcome page image
+    var welcomeImage: UIImageView!
+    // welcome page label
+    var welcomeLabel: UILabel!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .white
+        setupUIComponents()
+        initConstraints()
+    }
+    
+    func setupUIComponents() {
+        welcomeImage = UIElementUtil.createAndAddImageView(to: self, imageName: "airplane.departure", color: .link)
+        welcomeLabel = UIElementUtil.createAndAddLabel(to: self, text: "Welcome to Travel Oracle", fontSize: Constants.FONT_REGULAR, isCenterAligned: true, isBold: true, textColor: .black)
+    }
+    
+    func initConstraints() {
+        NSLayoutConstraint.activate([
+            welcomeImage.topAnchor.constraint(equalTo: self.safeAreaLayoutGuide.topAnchor, constant: Constants.VERTICAL_MARGIN_LARGE),
+            welcomeImage.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor),
+            welcomeImage.widthAnchor.constraint(equalToConstant: IMAGE_WIDTH),
+            welcomeImage.heightAnchor.constraint(equalToConstant: IMAGE_HEIGHT),
+        
+            welcomeLabel.topAnchor.constraint(equalTo: welcomeImage.bottomAnchor, constant: Constants.VERTICAL_MARGIN_LARGE),
+            welcomeLabel.centerXAnchor.constraint(equalTo: self.safeAreaLayoutGuide.centerXAnchor),
+            welcomeLabel.leadingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.leadingAnchor, constant: Constants.HORIZONTAL_MARGIN_REGULAR),
+            welcomeLabel.trailingAnchor.constraint(equalTo: self.safeAreaLayoutGuide.trailingAnchor, constant: -Constants.HORIZONTAL_MARGIN_REGULAR)
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+}
---
+++ CS5520-Traverl-Oracle/Screens/WelcomeScreen/WelcomeViewController.swift
@@ -0,0 +1,71 @@
+//
+//  WelcomeViewController.swift
+//  CS5520-Traverl-Oracle
+//
+//  Created by Changxu Ren on 11/18/23.
+//
+
+import UIKit
+import FirebaseAuth
+
+class WelcomeViewController: UIViewController {
+
+    // delay time
+    let DELAY_TIME = 1.0
+    
+    // initialize welcome view
+    let welcomeView = WelcomeView()
+    
+    // firebase authentication
+    var authStateDidChangeListenerHandle: AuthStateDidChangeListenerHandle?
+    
+    // add welcome view to view controller
+    override func loadView() {
+        view = welcomeView
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Add auth state listener with delay
+        authStateDidChangeListenerHandle = Auth.auth().addStateDidChangeListener { [weak self] (auth, user) in
+            guard let self = self else {
+                return
+            }
+            
+            DispatchQueue.main.asyncAfter(deadline: .now() + self.DELAY_TIME) {
+                if user != nil {
+                    self.transitionToHomeScreen()
+                } else {
+                    self.transitionToLoginPage()
+                }
+            }
+        }
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        // Remove auth state listener
+        if let handle = authStateDidChangeListenerHandle {
+            Auth.auth().removeStateDidChangeListener(handle)
+        }
+    }
+    
+    func transitionToHomeScreen() {
+        print("Transition to Home screen.")
+        let conversationsViewController = ConversationsViewController()
+        var viewControllers = self.navigationController!.viewControllers
+        viewControllers.removeAll()
+        viewControllers.append(conversationsViewController)
+        self.navigationController?.setViewControllers(viewControllers, animated: true)
+    }
+    
+    func transitionToLoginPage() {
+        print("Transition to Login screen.")
+        let loginViewController = LoginViewController()
+        var viewControllers = self.navigationController!.viewControllers
+        viewControllers.removeAll()
+        viewControllers.append(loginViewController)
+        self.navigationController?.setViewControllers(viewControllers, animated: true)
+    }
+}
