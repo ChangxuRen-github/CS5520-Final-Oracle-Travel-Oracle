d40fd8e7baab903cafa644c53dfde5f4c03883b3
ChangxuRen-github <changxuren.email@gmail.com>
2023-11-28 13:22:25 -0800

    Add store detail view and view controller

---
+++ CS5520-Traverl-Oracle/Screens/StoreDetailScreen/StoreDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  StoreDetailView.swift
+//  CS5520-Traverl-Oracle
+//
+//  Created by Changxu Ren on 11/28/23.
+//
+
+import UIKit
+
+class StoreDetailView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
